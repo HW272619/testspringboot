c83f9ff7d86917b75e838ca6a6a5adf83b2494eb
huwei <huwei@chalco-steering.com>
2020-09-23 12:36:59 +0800

    修改了测试功能

--- src/main/java/com/example/testspringboot/controller/TestController.java
+++ src/main/java/com/example/testspringboot/controller/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
     @RequestMapping (value = "/test",method = RequestMethod.GET)
     @ResponseBody
     public String test(){
-        return "hello";
+        return "helloworld";
     }
 
 }
